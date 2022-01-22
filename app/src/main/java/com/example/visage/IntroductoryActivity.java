ceff9202c96e47444dde2071573ea306e2ddefe5
Chithra <chitraravi012@gmail.com>
2022-01-22 11:47:57 +0530

    commit2

---
+++ app/src/main/java/com/example/visage/IntroductoryActivity.java
@@ -0,0 +1,33 @@
+package com.example.visage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class IntroductoryActivity extends AppCompatActivity {
+
+    ImageView logo,appName,splashImg;
+    LottieAnimationView lottieAnimationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introductory);
+
+        logo= findViewById(R.id.logo1);
+        appName= findViewById(R.id.app_name);
+        splashImg= findViewById(R.id.bg1);
+        logo= findViewById(R.id.lottie);
+
+        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        appName.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+
+
+    }
+}
