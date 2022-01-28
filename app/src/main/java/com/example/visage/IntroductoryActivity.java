0c3be4945eb3e8f990b0d7ccd91c7af5469f3494
Chithra <chitraravi012@gmail.com>
2022-01-28 21:22:18 +0530

    commit2

--- app/src/main/java/com/example/visage/IntroductoryActivity.java
+++ app/src/main/java/com/example/visage/IntroductoryActivity.java
@@ -1,8 +1,17 @@
 package com.example.visage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
+import android.widget.Adapter;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -12,22 +21,58 @@ public class IntroductoryActivity extends AppCompatActivity {
     ImageView logo,appName,splashImg;
     LottieAnimationView lottieAnimationView;
 
+    public static final int NUM_PAGES= 3;
+    private ViewPager viewPager;
+    private ScreenSlidePagerAdapter pageAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introductory);
 
-        logo= findViewById(R.id.logo1);
-        appName= findViewById(R.id.app_name);
-        splashImg= findViewById(R.id.bg1);
-        lottieAnimationView= findViewById(R.id.lottie);
+        logo = findViewById(R.id.logo1);
+        appName = findViewById(R.id.app_name);
+        splashImg = findViewById(R.id.bg1);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        viewPager = findViewById(R.id.pager);
+        pageAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
 
         splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
         logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
         appName.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
 
 
+        public ScreenSlidePagerAdapter(@NonNull FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    OnBoardingFragment1 tab1 = new OnBoardingFragment1();
+                    return tab1;
+                case 1:
+                    OnBoardingFragment2 tab2 = new OnBoardingFragment2();
+                    return tab2;
+                case 2:
+                    OnBoardingFragment3 tab3 = new OnBoardingFragment3();
+                    return tab3;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
