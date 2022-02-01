cfe930a545554c3d3c00007d449b929a9d2f0822
Chithra <chitraravi012@gmail.com>
2022-02-02 01:09:43 +0530

    commit2

--- app/src/main/java/com/example/visage/Login_Activity.java
+++ app/src/main/java/com/example/visage/Login_Activity.java
@@ -15,12 +15,23 @@ import android.widget.TextView;
 
 public class Login_Activity extends AppCompatActivity {
 
+    TextView sign_up;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        sign_up = findViewById(R.id.sign_up);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login_Activity.this, Registration_Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
 
